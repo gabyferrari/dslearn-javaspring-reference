88540a490e674f94993ab65bf32f271d26034bbc
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-09 12:57:12 -0300

    Offer entity

--- src/main/java/com/devsuperior/dslearn/entities/Course.java
+++ src/main/java/com/devsuperior/dslearn/entities/Course.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dslearn.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +20,9 @@ public class Course {
 	private String name;
 	private String imgUri;
 	private String imgGrayUri;
+	
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
 	
 	public Course() {
 		
@@ -58,6 +65,10 @@ public class Course {
 
 	public void setImgGrayUri(String imgGrayUri) {
 		this.imgGrayUri = imgGrayUri;
+	}
+
+	public List<Offer> getOffers() {
+		return offers;
 	}
 
 	@Override
