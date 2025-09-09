b33c1508a666725686397d95b89027b67fee34f5
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-09 18:48:50 -0300

    Reply entity

--- src/main/java/com/devsuperior/dslearn/entities/Reply.java
+++ src/main/java/com/devsuperior/dslearn/entities/Reply.java
@@ -1,9 +1,7 @@
 package com.devsuperior.dslearn.entities;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.Column;
@@ -15,17 +13,15 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_topic")
-public class Topic {
+@Table(name = "tb_reply")
+public class Reply {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String title;
 	
 	@Column(columnDefinition = "TEXT")
 	private String body;
@@ -34,41 +30,29 @@ public class Topic {
 	private Instant moment;
 	
 	@ManyToOne
-	@JoinColumn(name = "lesson_id")
-	private Lesson lesson;
-	
-	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
 	
 	@ManyToOne
 	@JoinColumn(name = "author_id")
 	private User author;
 	
 	@ManyToMany
-	@JoinTable(name = "tb_topic_likes",
-		joinColumns = @JoinColumn(name = "topic_id"),
-		inverseJoinColumns = @JoinColumn(name = "user_id"))	
+	@JoinTable(name = "tb_reply_likes",
+		joinColumns = @JoinColumn(name = "reply_id"),
+		inverseJoinColumns = @JoinColumn(name = "user_id")
+	)
 	private Set<User> likes = new HashSet<>();
-	
-	@OneToMany(mappedBy = "topic")
-	private List<Reply> replies = new ArrayList<>();
-	
-	@ManyToOne
-	@JoinColumn(name = "reply_id")
-	private Reply answer;
-	
-	public Topic() {
+
+	public Reply() {
 		
 	}
 
-	public Topic(Long id, String title, String body, Instant moment, Lesson lesson, Offer offer, User author) {
+	public Reply(Long id, String body, Instant moment, Topic topic, User author) {
 		this.id = id;
-		this.title = title;
 		this.body = body;
 		this.moment = moment;
-		this.lesson = lesson;
-		this.offer = offer;
+		this.topic = topic;
 		this.author = author;
 	}
 
@@ -78,14 +62,6 @@ public class Topic {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
 	}
 
 	public String getBody() {
@@ -104,20 +80,12 @@ public class Topic {
 		this.moment = moment;
 	}
 
-	public Lesson getLesson() {
-		return lesson;
+	public Topic getTopic() {
+		return topic;
 	}
 
-	public void setLesson(Lesson lesson) {
-		this.lesson = lesson;
-	}
-
-	public Offer getOffer() {
-		return offer;
-	}
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
+	public void setTopic(Topic topic) {
+		this.topic = topic;
 	}
 
 	public User getAuthor() {
@@ -130,18 +98,6 @@ public class Topic {
 
 	public Set<User> getLikes() {
 		return likes;
-	}
-
-	public Reply getAnswer() {
-		return answer;
-	}
-
-	public void setAnswer(Reply answer) {
-		this.answer = answer;
-	}
-
-	public List<Reply> getReplies() {
-		return replies;
 	}
 
 	@Override
@@ -160,7 +116,7 @@ public class Topic {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Topic other = (Topic) obj;
+		Reply other = (Reply) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
