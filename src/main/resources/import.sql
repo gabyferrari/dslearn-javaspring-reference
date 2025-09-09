8df5c70f1babc94378584cb6f872db7a2a3a9bf9
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-09-09 13:20:43 -0300

    Resource entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,3 +17,9 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://static-assets.codecademy.com/assets/course-landing-page/meta/1x1/learn-html.jpg', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://static-assets.codecademy.com/assets/course-landing-page/meta/1x1/learn-html.jpg', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Lives Bônus', 'Lives exclusivas para a turma', 3, 'https://static-assets.codecademy.com/assets/course-landing-page/meta/1x1/learn-html.jpg', 0, 1);
+
+
